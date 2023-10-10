8180394f63ec35ac9f8f27b310bdcd20271fde15
Jaime Pineda <jpin730@pm.me>
2023-10-10 16:34:01 -0600

    chore: :wrench: configure material ui

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@/styles/globals.css'
+
+import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { lightTheme } from '@/themes'
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
